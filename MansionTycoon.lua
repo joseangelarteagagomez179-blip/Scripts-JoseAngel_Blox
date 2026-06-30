4d72dc3d289d2838d9c5bdc2aff81f691fa64bf5
Jose Angel <joseangelarteagagomez179@gmail.com>
2026-06-30 14:54:25 -0400

    Add functionality to display contents of Plot1
    Updated script to print contents of Plot1 in Tycoons.

--- MansionTycoon.lua
+++ MansionTycoon.lua
@@ -1,10 +1,10 @@
--- VER LO QUE HAY DENTRO DE TYCOONS
-print("=== CONTENIDO DE TYCOONS ===")
-local Tycoons = game.Workspace:FindFirstChild("Tycoons")
-if Tycoons then
-    for _, cosa in pairs(Tycoons:GetChildren()) do
-        print("📂 " .. cosa.Name)
+-- VER LO QUE HAY DENTRO DE PLOT1
+print("=== CONTENIDO DE PLOT1 ===")
+local Plot = game.Workspace.Tycoons:FindFirstChild("Plot1") -- Cambia a Plot2 o Plot3 si no estás en el 1
+if Plot then
+    for _, objeto in pairs(Plot:GetChildren()) do
+        print("📦 " .. objeto.Name)
     end
 else
-    print("❌ No se encontró la carpeta Tycoons")
+    print("❌ No se encontró el Plot")
 end
